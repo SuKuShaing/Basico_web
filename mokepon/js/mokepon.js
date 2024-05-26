606146d50ee99e079ea14a8d5a1c815c28a05195
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-05-26 18:23:35 -0400

    Edu(Mokepon): se habilita el btn reiniciar juego y recarga la página, además se desactivan los botones de ataque

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -3,6 +3,10 @@ let ataqueEnemigo
 let vidasJugador = 3;
 let vidasEnemigo = 3;
 
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById("boton-mascota");
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);  
@@ -13,6 +17,9 @@ function iniciarJuego() {
     botonAgua.addEventListener("click", ataqueAgua);
     let botonTierra = document.getElementById("boton-tierra");
     botonTierra.addEventListener("click", ataqueTierra);
+
+    let botonReiniciar = document.getElementById("boton-reiniciar");
+    botonReiniciar.addEventListener("click", reiniciarJuego);
 };
 
 function seleccionarMascotaJugador() {
@@ -115,13 +122,21 @@ function crearMensajeFinal(resultadoFinal) {
     parrafo.innerHTML = `<strong>${resultadoFinal}</strong>`;
     
     sectionMensajes.appendChild(parrafo);
+
+
+    let botonFuego = document.getElementById("boton-fuego");
+    botonFuego.disabled = true
+    let botonAgua = document.getElementById("boton-agua");
+    botonAgua.disabled = true
+    let botonTierra = document.getElementById("boton-tierra");
+    botonTierra.disabled = true
 };
 
-function aleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+
+
+function reiniciarJuego() {
+    location.reload();
 };
-
-
 
 window.addEventListener("load", iniciarJuego);
 
