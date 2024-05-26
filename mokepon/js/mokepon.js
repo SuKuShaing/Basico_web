90c78da049a33d1ff85a643a11117e5e7dedd696
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-05-26 17:54:23 -0400

    Edu(Mokepon): se hace el sistema para que pierda pidas en el combate

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,5 +1,7 @@
 let ataqueJugador 
 let ataqueEnemigo
+let vidasJugador = 3;
+let vidasEnemigo = 3;
 
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById("boton-mascota");
@@ -64,14 +66,22 @@ function ataqueAleatorioEnemigo() {
 };
 
 function combate() {
+    let spanVidasJugador = document.getElementById("vidas-jugador");
+    let spanVidasEnemigo = document.getElementById("vidas-enemigo");
+
+
     if (ataqueEnemigo == ataqueJugador) { // Empate
         crearMensaje("Empate");
     }
     else if (ataqueEnemigo == 'Fuego' && ataqueJugador == 'Agua' || ataqueEnemigo == 'Agua' && ataqueJugador == 'Tierra' || ataqueEnemigo == 'Tierra' && ataqueJugador == 'Fuego') { 
         crearMensaje("Ganaste");
+        vidasEnemigo--;
+        spanVidasEnemigo.innerHTML = vidasEnemigo;
     }
     else {
         crearMensaje("Perdiste");
+        vidasJugador--;
+        spanVidasJugador.innerHTML = vidasJugador;
     }
 };
 
