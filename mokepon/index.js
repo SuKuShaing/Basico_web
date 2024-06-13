c787e46a0648c184f74d0e7edf8b551f726b0acd
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-06-13 13:05:39 -0400

    creamos el primer servidor

--- mokepon/index.js
+++ mokepon/index.js
@@ -1,1 +1,11 @@
-console.log("Hola Node")
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hola usuario');
+});
+
+app.listen(8080, () => {
+    console.log("El servidor está escuchando");
+});
